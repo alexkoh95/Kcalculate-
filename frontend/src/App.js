93c8cb305f4c55cce2e952696181ed8523db503b
imim <im.imanmaliki@gmail.com>
2021-09-24 14:07:46 +0800

    Main Components Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import SideNavBar from './components/SideNavBar';
+import Dashboard from './components/Dashboard/Dashboard';
+import Log from './components/Log/Log';
+import History from './components/LogHistory/History';
+import Settings from './components/Settings/Settings';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className=""><SideNavBar /></div>
+      <main className="mx-4 p-9 pl-64">          
+          <Switch>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/log" exact component={Log} />
+            <Route path="/loghistory" exact component={History} />
+            <Route path="/settings" exact component={Settings} />
+
+          </Switch>
+
+      </main>
+      </div>
+      </Router>
   );
 }
 
