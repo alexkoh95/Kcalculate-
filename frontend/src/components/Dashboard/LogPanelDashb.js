d5abf1edceb0a1ba49eb975431b4bec96a505514
imim <im.imanmaliki@gmail.com>
2021-09-30 00:19:58 +0800

    Settings Page UI updated

--- frontend/src/components/Dashboard/LogPanelDashb.js
+++ frontend/src/components/Dashboard/LogPanelDashb.js
@@ -45,8 +45,11 @@ const LogPanelDashb = ({ todayMeals }) => {
                 <div className="col-span-2 uppercase tracking-wider text-left my-auto text-xs my-auto">  Snack </div>
                 <div className="text-md my-auto text-right text-indigo-600"> {snackKcal}kcal </div></div>
             
-                <div className="items-center justify-center bg-base border-indigo-600 border-2 border-dashed py-2 px-4 text-indigo-600 rounded-md m-3 opacity-50 hover:opacity-100 hover:border-solid">
-                <Link to="/log" className="col-span-2 uppercase tracking-wode text-center my-auto text-xs font-medium my-auto">  + Add Item </Link>
+                <div className="block flex items-center justify-center bg-base border-indigo-600 border-2 border-dashed py-2 px-4 text-indigo-600 rounded-md m-3 opacity-50 hover:opacity-100 hover:border-solid">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 my-auto transform inline-flex" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span className="inline-flex ml-1"><Link to="/log" className="col-span-2 uppercase tracking-wode text-center my-auto text-xs font-medium my-auto"> Add Item </Link></span>
                 </div>
         
         </div>
--- frontend/src/components/Settings/SettingsPage.js
+++ frontend/src/components/Settings/SettingsPage.js
@@ -7,10 +7,10 @@ const SettingsPage = ({ user }) => {
     return (
         <div>
             <div className=""><SideNavBar /></div>
-            <main className="mx-4 p-9 pl-64">
-                <div className="grid grid-cols-3 space-x-5">
+            <main className="mx-4 p-9 pl-64 pt-12">
+                <div className="grid grid-cols-3 space-x-7">
                     <div className="col-span-2">
-                        {/* <Settings user={user} /> */}
+                        <Settings user={user} />
                     </div>
 
 
